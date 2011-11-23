6760556ba92878bf2302e92610a6f777038caeb9
unknown <barbe@BARBE.gi.fr>
2011-11-23 14:53:41 +0100

    Change to 0.9

--- README.md
+++ README.md
@@ -6,15 +6,19 @@ A lite but powerful javascript template engine
 ### Basics
 Simply load **tlite** on your page
 
-T-Lite work with two arguments:
+T-Lite work with two methods:
 
-* your template
-* the object containing vars and other elements needed by the template
+* Find : replace vars in string (more fast for small template without loops/conditions)
+* Parse : find/replace var and do loops/conditions
+
+Each method take two arguments :
+* A template string
+* A var object
 
 In a template, a var is represented in that syntax : `{varName}`. If your var is an object, you can use the normal javascript notation to go down your object : `{varName.otherElement.deepInTheObject}`. The var object can contain any kind of elements: vars, object, array, function... Functions are automatically called when needed.
 
 ```javascript
-tlite('Your small template with a {something.in.big.object}', { something : {in: {big: {object: 'tlite'}}}});
+Tlite.find('Your small template with a {something.in.big.object}', { something : {in: {big: {object: 'tlite'}}}});
 ```
 
 ### Condition
@@ -23,16 +27,16 @@ Condition can check if a var exists on the object, check the equality... Like va
 #####Simple if
 
 ```javascript
-tlite('{if name}Hello {name}{/if}', { name: 'Mousse' });
+Tlite.parse('{if name}Hello {name}{/if}', { name: 'Mousse' });
 ```
 
 #####Simple if/else
 
 ```javascript
 // boolean var
-tlite('{if name}Hello {name} !{else}Hello unknown !{/if}', { name: false });
+Tlite.parse('{if name}Hello {name} !{else}Hello unknown !{/if}', { name: false });
 // function var
-tlite('{if name}Hello {name} !{else}Hello unknown !{/if}', { name: function(){ return 'James' });
+Tlite.parse('{if name}Hello {name} !{else}Hello unknown !{/if}', { name: function(){ return 'James' });
 ```
 
 ##### Complex if/else
@@ -42,28 +46,28 @@ Complex if/else can use comparison char in this list : `===, ==, !=, <=, >=`.
 
 ```javascript
 // simple var
-tlite('{if age != 60 }Hello {name} !{else}Hello old men !{/if}', { name: 'James', age : 21});
-tlite('{if age != medium }Hello {name} !{else}Hello old men !{/if}', { name: 'James', age : 21, medium : 60});
+Tlite.parse('{if age != 60 }Hello {name} !{else}Hello old men !{/if}', { name: 'James', age : 21});
+Tlite.parse('{if age != medium }Hello {name} !{else}Hello old men !{/if}', { name: 'James', age : 21, medium : 60});
 ```
 
 ```javascript
 //function var
-tlite('{if age != 60 }Hello {name} !{else}Hello old men !{/if}', { name: 'James', age : function(){ return this.person.age; }});
+Tlite.parse('{if age != 60 }Hello {name} !{else}Hello old men !{/if}', { name: 'James', age : function(){ return this.person.age; }});
 ```
 
 ### For
 For automatically made a loop thought a given array/object. For give a key and value var to simply use result :
 
 ```javascript
-tlite('{for loop}{key}:{value}<br>{/for}', {loop:['First', 'Second', 'Third']});
-tlite('{for loop}{key}:{value}<br>{/for}', {loop:{name: 'Paul', age: 24, city: 'Paris'}});
+Tlite.parse('{for loop}{key}:{value}<br>{/for}', {loop:['First', 'Second', 'Third']});
+Tlite.parse('{for loop}{key}:{value}<br>{/for}', {loop:{name: 'Paul', age: 24, city: 'Paris'}});
 ```
 
 ###### Filters
 Filters can remove one or multiple var from the current object. Each element you want to filter will be separated by a pipe:
 
 ```javascript
-tlite('{for loop|not|that}{key}:{value}<br>{/for}', {loop:{name: 'Paul', age: 24, not: 'Paris', that: 'Test'}});
+Tlite.parse('{for loop|not|that}{key}:{value}<br>{/for}', {loop:{name: 'Paul', age: 24, not: 'Paris', that: 'Test'}});
 ```
 
 ### Function
@@ -71,6 +75,10 @@ tlite('{for loop|not|that}{key}:{value}<br>{/for}', {loop:{name: 'Paul', age: 24
 Vars containin a function will receive a context argument, representing the current level of parsed element. In a for, you can access to the *key* and *value* vars.
 
 ## Versions
+
+#### 0.9
+* Expose API
+* Improve compression
 
 #### 0.8
 * Improve if/else performances
--- tlite.js
+++ tlite.js
@@ -1,64 +1,51 @@
-//      Version : 0.8
+//      Version : 0.9
 //     (c) 2011 Jérémy Barbe.
 //     May be freely distributed under the MIT license.
 
-window['tlite'] = function(template, vars){
-    var context = vars,
+window['Tlite'] = new (function() {
+    var _this = this,
+        topContext,
+        curContext,
         elseString = "{else}",
-        ifCache = {},
-        ifType = {
-            '<=' : 'lw',
-            '>=' : 'gt',
-            '==' : 'eq',
-            '===': 'sq',
-            '!=' : 'nt'
-        };
+        ifCache = {};
 
     /**
      * Find the value asked in the template in the current context
-     * @param elem      the elet to search
+     * @param {String} elem      the elemnt to search
      */
-     function findValue(elem){
+    function findValue(elem){
         var path = elem.split('.'),
-            value = context[path.shift()];
+            value = curContext[path.shift()];
 
-        while(value != undefined && path.length){
+        while (value != undefined && path.length) {
             value = value[path.shift()];
         }
 
-        return value == undefined ? elem : value.call ? value.call(this, context) : value;
+        return undefined == value ? elem : value.call ? value.call(this, curContext) : value;
     }
 
     /**
-     * replace template var to their value
-     * @param tpl           the template to parse
+     * Parse current string  to find If/Else
+     * @param {String} tpl
      */
-    function find(tpl){
-        return tpl.replace(/\{(.+?)\}/g, function(foundVar ,varContent){
-            return findValue(varContent);
-        });
-    }
-
-    function parseIf(tpl){
-        return tpl.replace(/\{if (.*?)\}(.*)\{\/if\}/, function(string, condition, result, elseResult, type){
+    function parseIf(tpl) {
+        return tpl.replace(/\{if (.*?)\}(.*)\{\/if\}/, function(string, condition, result, elseResult, type) {
 
             elseResult = result.split(elseString);
 
             elseResult[1] ?
-               (elseResult = elseResult.pop(), result = result.split(elseString+elseResult)[0]) :
-               elseResult = 0;
+                (elseResult = elseResult.pop(),result = result.split(elseString + elseResult)[0]) :
+                elseResult = 0;
 
             // else, it's a complex condition, need to parse first and last element
             condition = condition.split(' ');
-            type = ifType[condition[1]];
+            type = ""+condition[1];
 
-
-            if(type && !ifCache[type]){
-                ifCache[type] = buildCondition("return x "+condition[1]+" y");
+            if (condition[1] && !ifCache[type]) {
+                ifCache[type] = buildCondition("return x " + condition[1] + " y");
             }
 
-            type = type ? ifCache[type](findValue(condition[0]), findValue(condition[2])):
-                          findValue(condition[0]);
+            type = condition[1] ? ifCache[type](findValue(condition[0]), findValue(condition[2])) : findValue(condition[0]);
 
             return type ? parse(result) : (elseResult ? parse(elseResult) : '')
         });
@@ -66,61 +53,82 @@ window['tlite'] = function(template, vars){
 
     /**
      * Create a function to compare to args
-     * @param content   content of the function
+     * @param {String|null} content content of the function
      */
-    function buildCondition(content){
+    function buildCondition(content) {
         return new Function("x", "y", content);
     }
 
 
-    function parseFor(tpl){
-        return tpl.replace(/\{for (.*?)(\|(.*?))?\}(.+)\{\/for\}/, function(string, forVar, filterString, filter, tpl){
+    function parseFor(tpl) {
+        return tpl.replace(/\{for (.*?)(\|(.*?))?\}(.+)\{\/for\}/, function(string, forVar, filterString, filter, tpl) {
             return makeFor(findValue(forVar), filter, tpl);
         });
     }
 
     /**
      * Do a for loop
-     * @param forVar        the var used for the loop
-     * @param filterString  the var in the loop filtered at render
-     * @param content       content of the loop
+     * @param {Array|Object} forVar        the var used for the loop
+     * @param {String|null} filterString  the var in the loop filtered at render
+     * @param {String} content       content of the loop
      */
-    function makeFor(forVar, filterString, content){
+    function makeFor(forVar, filterString, content) {
         var forReturn = '', i, result;
 
-        filterString = (filterString || '')+'|value|key';
+        filterString = (filterString || '') + '|value|key';
 
-        for(i in forVar){
-            if(forVar.hasOwnProperty(i) && filterString.search(i) < 0){
-                result = typeof forVar[i] == "object" ?  forVar[i] : {};
+        for (i in forVar) {
+            if (forVar.hasOwnProperty(i) && filterString.search(i) < 0) {
+                result = typeof forVar[i] == "object" ? forVar[i] : {};
 
                 result.value = forVar[i];
                 result.key = i;
 
-                context = result;
+                curContext = result;
                 forReturn += parse(content);
             }
         }
-        
+
         return forReturn;
     }
 
     /**
      * Parse the current template to find var/condition/for
-     * @param tpl
+     * @param {String} tpl
      */
     function parse(tpl){
-        //preserve current context
-        var curContext = context;
-        
+        var context = curContext;
         tpl.search('{if') < tpl.search('{for') ?
-            tpl = parseFor(parseIf(tpl)):
+            tpl = parseFor(parseIf(tpl)) :
             tpl = parseIf(parseFor(tpl));
 
         //reset current context
-        context = curContext;
-        return find(tpl);
+        curContext = context;
+        return _this.find(tpl);
     }
 
-    return parse(template);
-}
+    /**
+     * replace template var to their value
+    * @param {String} tpl
+    * @param {Object} context
+     */
+    _this.find = function(tpl, context) {
+        context&&(curContext = context)
+        
+        return tpl.replace(/\{(.+?)\}/g, function(foundVar, varContent) {
+            return findValue(varContent);
+        });
+    }
+
+    /**
+     * Exposed parse, run template parsing
+     * @param {String} tpl
+     * @param {Object} context
+     */
+    _this.parse = function(tpl, context) {
+        topContext = topContext || context;
+        curContext = context;
+        
+        return parse(tpl);
+    }
+})();
